ce1b63e4eac3b8330ec3bb7c56a019327e2b8f67
dotcom29 <dotcom29102001@gmail.com>
2025-03-19 06:11:07 +0700

    add JSON

--- Test/Test.js
+++ Test/Test.js
@@ -1,3 +1,13 @@
-function showMessage() {
-    alert('This is External script')
+const timeElement = document.getElementById("time")
+
+function gettime(){
+    const time = new Date()
+    const hours = time.getHours()
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    timeElement.innerHTML = 
+    `${hours <10 ? `0${hours}` : hours} : 
+    ${minutes <10 ? `0${minutes}` : minutes} : 
+    ${seconds <10 ? `0${seconds}` : seconds}`
 }
+setInterval(gettime, 1000);
